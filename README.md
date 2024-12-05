d5d4eb61d4ca479c3f2eccb7f3ad38f500d28e31
Dragos GALALAE <kdraga@westermo.se>
2024-12-05 10:59:02 +0200

    Add recovery with PS_ON and led blinking according to state

--- README.md
+++ README.md
@@ -6,10 +6,11 @@ The author assumes no liability which may come from using the software or by fol
 
 It is using platform.io and written using C language.
 All the build requirements should be automatically handled by platform.io.
+Development environment assumes you are using vscode with platform.io installed and have a 5V serial adapter to flash the MCU.
 
-To flah MCU (assumes vscode with platform.io installed):
-1) connect 5V serial adapter to board 4 pin header reversing TX and RX
-   Do not connect the 5V pin!
+To flash MCU:
+0) the relay board should have no power
+1) connect 5V serial adapter to board 4 pin header reversing TX and RX, do not connect the 5V pin!
 2) put a jumper on the reset header.
 3) power on the relay board (high voltage is present!)
 4) click "Upload" in platform.io controls, a new terminal will appear
@@ -18,10 +19,20 @@ To flah MCU (assumes vscode with platform.io installed):
 
 Current features:
 - response time: 100ms - changes in conditions faster than this won't be detected
-- startup delay: 45s - the relay will not trip is PS_ON is disconnected (or 0) and power has just been applied
-- power off delay: 15s - time to allow the board/PC to shutdown cleanly
+- startup delay: 60s - the relay will not trip is PS_ON is disconnected (or 0) and power has just been applied
+- power off delay: 20s - time to allow the board/PC to shutdown cleanly
+- recovery delay: 5s - recover with PS_ON after power off, blanking time to avoid fast power off/on cycle
+
+Leds show state:
+- green off and red off - no power to the relay
+- green on and red on -  MCU is reset or in programming mode
+- green on and red blinking - startup delay, power to system, only SC is available
+- green on and red off - power to the system
+- green blinking and red off - PS_ON triggered, power off delay
+- green off and red on - no power to the system, relay is in power off state
+- green alternate red blinking - SC triggered
 
 More details in the C code.
 
-Relay information can be found here:
-https://github.com/bigtreetech/BIGTREETECH-Relay-V1.2
+Relay information can be found here: https://github.com/bigtreetech/BIGTREETECH-Relay-V1.2
+MCU vendor web page: https://www.stcmicro.com/STC/STC15W204S.html
--- src/main.c
+++ src/main.c
@@ -5,15 +5,30 @@
 #define LED_OFF     P30     /* Power off led (red) */
 #define LED_ON      P31     /* Power on led (green) */
 #define PS_DLY      P32     /* Delayed power off */
-#define SCP         (!P33)     /* Immediate power off (short protection) */
+#define SCP         (!P33)  /* Immediate power off (short protection) */
 #define RELAY       P55     /* Relay control (power to printer on/off switch) */
 
 #define RESPONSE_DELAY  100 /* how fast the sofware responds to input changes */
-#define START_DELAY_S   45  /* time at startup to ignore PS_DLY state */
-#define STOP_DELAY_S    15  /* time to wait before power off after trigger by PS_DLY */
+#define START_DELAY_S   60  /* time at startup to ignore PS_DLY state */
+#define STOP_DELAY_S    20  /* time to wait before power off after trigger by PS_DLY */
+#define RECOVER_DELAY_S 5   /* time to wait before power recovery after PS_DLY gets restored */
+#define BLINKER_MUL     3   /* used for blinking LEDs, multiple of RESPONSE_DELAY */      
+
+#if (RECOVER_DELAY_S > (STOP_DELAY_S-1))
+#error "RECOVER_DELAY_S must be at least 1 second smaller than STOP_DELAY_S"
+#endif
+
+#if (RESPONSE_DELAY > 500)
+#error "RESPONSE_DELAY must be lower than 500ms"
+#endif
+
+#if (BLINKER_MUL > 250)
+#error "BLINKER_MUL must be lower than 250"
+#endif
 
 #define PWUP_DLY_TICKS  (START_DELAY_S*1000UL/RESPONSE_DELAY*1UL)
 #define PWDN_DLY_TICKS  (STOP_DELAY_S*1000UL/RESPONSE_DELAY*1UL)
+#define PWRE_DLY_TICKS  (STOP_DELAY_S*1000UL/RESPONSE_DELAY*1UL)
 
 /* stated of the device */
 typedef enum
@@ -21,67 +36,128 @@ typedef enum
     powerup,    /* first startup/reset will ignore PS_DLY state */
     running,    /* normal running state */
     psdelay,    /* PS_DLY was triggered, but we wait for power off delay */
-    tripped,    /* PS_DLY or SCP trigger occurred */
-
+    tripped,    /* PS_DLY trigger occurred */
+    tripped_sc, /* SCP trigger occurred */
 }states_t;
 
 void main(void)
 {
     /* setup i/o */
-	P3M1 = 0xB4;
+    P3M1 = 0xB4;
     P3M0 = 0xB4;
-	P5M0 = 0xEA;
+    P5M0 = 0xEA;
     /* initial state of outputs */
     RELAY = 0;
-	LED_OFF = 0;
-	LED_ON = 0xff;
+    LED_OFF = 0;
+    LED_ON = 0;
     /* vars */
     states_t state = powerup;
     uint32_t updelay = 0;
+    uint8_t blink = 0;
     /* program loop */
-	while(1) {
+    while(1) {
         /* wait response time */
         delay_ms(RESPONSE_DELAY);
         /* check SCP, state independent */
         if (SCP)
-            state = tripped;
+            state = tripped_sc;
         /* check states */
         switch (state)
         {
         case powerup:
-            /* check power up delay */
+            /* powering state */
+            RELAY = 0;
+            /* increase delay timers */
             updelay += 1;
+            blink += 1;
+            if (blink > BLINKER_MUL) {
+                /* leds */
+                LED_OFF = (!LED_OFF);
+                LED_ON = 1;
+                blink = 0;
+            }
+            /* check power up delay */
             if (updelay > PWUP_DLY_TICKS) {
                 updelay = 0;
-                state = running;
+                /* check PS_DLY */
+                if (PS_DLY)
+                    state = running;
+                else
+                    state = tripped;
             }
             break;
         case running:
+            /* powering state */
+            RELAY = 0;
+            /* leds */
+            LED_OFF = 0;
+            LED_ON = 1;
+            /* reset timers */
             updelay = 0;
+            blink = 0;
             /* check PS_DLY */
             if (PS_DLY)
                 state = psdelay;
             break;        
         case psdelay:
-            /* check power up delay */
+            /* powering state */
+            RELAY = 0;
+            /* increase delay timers */
             updelay += 1;
+            blink += 1;
+            if (blink > BLINKER_MUL) {
+                /* leds */
+                LED_OFF = 0;
+                LED_ON = (!LED_ON);
+                blink = 0;
+            }
+            /* check power down delay */
             if (updelay > PWDN_DLY_TICKS) {
-                state = tripped;
-                LED_OFF = 0xff;
-                LED_ON = 0;
-            }
-#if 0
-            /* check PS_DLY */
-            if (!PS_DLY) {
+                /* reset timers */
+                blink = 0;
                 updelay = 0;
-                state = running;
+                state = tripped;
             }
-#endif
+            break;
+        case tripped:
+            /* tripped state */
+            RELAY = 1;
+            /* leds */
+            LED_OFF = 1;
+            LED_ON = 0;
+            /* increase delay timers */
+            updelay += 1;
+            /* power recovery */
+            if (updelay > PWRE_DLY_TICKS) {
+                /* check PS_DLY pin */
+                if (!PS_DLY) {
+                    /* reset timers */
+                    blink = 0;
+                    updelay = 0;
+                    state = running;
+                }
+                updelay = PWRE_DLY_TICKS;
+            }
             break;   
-     
         default:
             /* tripped state */
-            RELAY = 0xff;
+            RELAY = 1;
+            /* increase delay timers */
+            blink += 1;
+            /* blinking leds alternatively, one it BLINKER_MUL on, then the other is BLINKER_MUL on */
+            if (blink > (2*BLINKER_MUL))
+                blink=0;
+            if (blink > BLINKER_MUL) {
+                /* leds */
+                LED_OFF = 1;
+                LED_ON = 0;
+            } else {
+                /* leds */
+                LED_OFF = 0;
+                LED_ON = 1;
+            }
+            /* reset timers */
+            updelay = 0;
             break;
         }       
     }
